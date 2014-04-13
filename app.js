b1c8acb130e42bb036e5fd0ea94b70d8e8d4096f
pqhwan <hyun_sik_kim@brown.edu>
2014-04-13 17:04:45 -0400

    serves static files

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 //Get dependenies
 
 var express = require('express'),
-    colors = require('colors');
+    colors = require('colors').
+    pg = require('pg');
 
 var app = express();
 
 // environment variables
 app.set('port', process.env.PORT || 3000);
+// smallest middleware in the world
+app.use(function(req,res,next){
+    //console.log(__dirname);
+    next();
+});
+
+
+app.use(express.static(__dirname));
+//app.use('/assets/css',express.static(__dirname+'/assets/css'));
+//app.use('/assets/js',express.static(__dirname+'/assets/js'));
 //app.use(express.favicon());
 //app.use(express.methodOverride());
 //app.use(app.router);
@@ -16,22 +27,53 @@ app.set('port', process.env.PORT || 3000);
  * Page URLs.
  */
 
-//TODO send back splash page
+//TODO splash.html
 app.get('/',function(req,res){
-    res.send('Login page');
+    res.sendfile('index.html');
 }); 
 
-//TODO
-app.get('/signup',function(req,res){
-    res.send('Signup page');    
+//TODO signup.html (we don't have this yet)
+app.get('/login',function(req,res){
+    res.sendfile('login.html');
 }); //send back signup page
+
+//TODO main.html
+app.get('/nearby', function(req,res){
+    res.sendfile('main.html');
+});
+
+//TODO party create form (don't have this yet)
+app.get('/party/create', function(req,res){
+    res.send('party upload form');
+});
+
+//TODO party profile (don't have this yet)
+app.get('/party/:id',function(req,res){
+
+});
 
 
 
 /**
  * Action URLs.
  */
-app.post('/signup');
+
+app.post('/login',function(req,res){
+    //get credentials from req
+    //find it in db (callback for directing user to /nearby)
+});
+
+//TODO 
+app.post('/signup',function(req,res){
+    //get credentials from req
+    //store in db
+});
+
+//TODO get coordinate, send back nearby parties
+app.post('/nearby',function(req,res){
+    //get user coordinate from req
+    //store in db
+});
 
 //start app
 app.listen(app.get('port'));
